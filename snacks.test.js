7646a3c16ee2621b669c9850b5cc83d5dcf99a6a
nicolaparutto <nicolaparutto2003@gmail.com>
2025-05-06 18:06:30 +0200

    feat: enclosed the code in describe

--- snacks.test.js
+++ snacks.test.js
@@ -1,43 +1,49 @@
 const { getInitials, createSlug, average, secondCreateSlug, isPalindrome, thirdCreateSlug, findPostById } = require("./snacks")
 
-// 🏆 Snack 1:
-test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
-	expect(getInitials("Luca Rossi")).toBe("L,R")
+describe('Test sulle stringhe', () => {
+	// 🏆 Snack 1:
+	test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
+		expect(getInitials("Luca Rossi")).toBe("L,R")
+	})
+
+	// 🏆 Snack 2:
+	test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+		expect(createSlug("STRINGA")).toBe("stringa")
+	})
+
+	// 🏆 Snack 4:
+	test('La funzione secondCreateSlug sostituisce gli spazi con -.', () => {
+		expect(secondCreateSlug("Sostituisci gli spazi")).toBe("sostituisci-gli-spazi")
+	})
+
+	// 🏆 Snack 5:
+	test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
+		expect(isPalindrome("radar")).toBe("radar")
+	})
+
+	// 🏆 Snack 6:
+	test('La funzione thirdCreateSlug lancia un errore se il titolo è vuoto o non valido.', () => {
+		expect(() => thirdCreateSlug("")).toThrow()
+	})
 })
 
-// 🏆 Snack 2:
-test('La funzione createSlug restituisce una stringa in lowercase.', () => {
-	expect(createSlug("STRINGA")).toBe("stringa")
+describe('Test su conteggi numerici', () => {
+	// 🏆 Snack 3:
+	test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+		expect(average([2, 4, 6, 8])).toBe(5)
+	})
 })
 
-// 🏆 Snack 3:
-test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
-	expect(average([2, 4, 6, 8])).toBe(5)
-})
-
-// 🏆 Snack 4:
-test('La funzione secondCreateSlug sostituisce gli spazi con -.', () => {
-	expect(secondCreateSlug("Sostituisci gli spazi")).toBe("sostituisci-gli-spazi")
-})
-
-// 🏆 Snack 5:
-test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
-	expect(isPalindrome("radar")).toBe("radar")
-})
-
-// 🏆 Snack 6:
-test('La funzione thirdCreateSlug lancia un errore se il titolo è vuoto o non valido.', () => {
-	expect(() => thirdCreateSlug("")).toThrow()
-})
-
-// 🏆 Snack 7:
-test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
-	expect(findPostById(2))
-		.toEqual(
-			{
-				id: 2,
-				title: "Hobby",
-				slug: "pescare, mangiare, cantare"
-			}
-		)
+describe('Test su validazione oggetti', () => {
+	// 🏆 Snack 7:
+	test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+		expect(findPostById(2))
+			.toEqual(
+				{
+					id: 2,
+					title: "Hobby",
+					slug: "pescare, mangiare, cantare"
+				}
+			)
+	})
 })

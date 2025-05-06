4d40f75d1bb810c17e776717a635a8eefa06d03e
nicolaparutto <nicolaparutto2003@gmail.com>
2025-05-06 17:01:55 +0200

    feat: snack 2-3-4-5 completed

--- snacks.js
+++ snacks.js
@@ -6,11 +6,38 @@ const getInitials = (completeName) => {
 	return initialsJoined
 }
 
-
 // 🏆 Snack 2:
+const createSlug = (stringa) => {
+	return stringa.toLowerCase()
+}
+
 // 🏆 Snack 3:
+const average = (array) => {
+	const arraSum = array.reduce((acc, curr) => {
+		return acc + curr
+	}, 0)
+	return arraSum / array.length
+}
+
 // 🏆 Snack 4:
+const secondCreateSlug = (frase) => {
+	return frase.split(" ").join("-")
+}
+
+// 🏆 Snack 5:
+const isPalindrome = (word) => {
+	return word.split("").reverse().join("")
+}
+
+// 🏆 Snack 6:
+// 🏆 Snack 7:
+// 🏆 Snack 8:
+
 
 module.exports = {
-	getInitials
+	getInitials,
+	createSlug,
+	average,
+	secondCreateSlug,
+	isPalindrome
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials } = require("./snacks")
+const { getInitials, createSlug, average, secondCreateSlug, isPalindrome } = require("./snacks")
 
 // 🏆 Snack 1:
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
@@ -6,5 +6,24 @@ test('La funzione getInitials restituisce le iniziali di un nome completo.', () 
 })
 
 // 🏆 Snack 2:
+test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+	expect(createSlug("STRINGA")).toBe("stringa")
+})
+
 // 🏆 Snack 3:
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+	expect(average([2, 4, 6, 8])).toBe(5)
+})
+
 // 🏆 Snack 4:
+test('La funzione secondCsreateSlug sostituisce gli spazi con -.', () => {
+	expect(secondCreateSlug("sostituisci gli spazi")).toBe("sostituisci-gli-spazi")
+})
+
+// 🏆 Snack 5:
+test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
+	expect(isPalindrome("radar")).toBe("radar")
+})
+
+// 🏆 Snack 6:
+// 🏆 Snack 7:
